5a289222370bbb52002603ba5adc11b65e510c4f
Erkebulan Safiolla <erkebulan91@gmail.com>
2022-08-19 02:57:34 +0600

    адаптивка

--- app/js/main.js
+++ app/js/main.js
@@ -11,5 +11,52 @@ $('.form-group.light input').blur(function (){
 $('.catalog-link').click(function (e){
   e.preventDefault();
   $(this).toggleClass('active');
-  $('.catalog').toggleClass('show')
+  $('.catalog').toggleClass('show');
 });
+$('.close-catalog').click(function (){
+  $('.catalog-link').removeClass('active');
+  $('.catalog').removeClass('show');
+  $('.header-row-toggle').removeClass('active');
+  $('.header-main-row').removeClass('show');
+  $('body').removeClass('scroll-locked');
+})
+
+//поиск в header
+$('.search-btn').click(function (){
+  $('.header-search-form').addClass('show');
+});
+$('.close-header-search').click(function (){
+  $('.header-search-form').removeClass('show');
+  $('.header-search-form input').val('');
+  $('.header-search-dropdown').removeClass('show');
+});
+
+$('.header-search-form input').on('input', function (){
+  if($(this).val().length > 0) {
+    $('.header-search-dropdown').addClass('show');
+  } else {
+    $('.header-search-dropdown').removeClass('show');
+  }
+});
+
+//поиск в мобильном header
+$('.close-search-xs').click(function (){
+  $('.search-xs-form').removeClass('show');
+  $('.search-xs-form input').val('');
+  $('.search-xs-dropdown').removeClass('show');
+});
+
+$('.search-xs-form input').on('input', function (){
+  if($(this).val().length > 0) {
+    $('.search-xs-dropdown').addClass('show');
+  } else {
+    $('.search-xs-dropdown').removeClass('show');
+  }
+});
+
+//меню header в мобилке
+$('.header-row-toggle').click(function (){
+  $(this).toggleClass('active');
+  $('.header-main-row').toggleClass('show');
+  $('body').toggleClass('scroll-locked');
+})
